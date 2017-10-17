f42969279e0b265c735176fe0c5f636fb2aa76f2
LeoXugu <robinleoknauth@gmail.com>
2017-10-16 22:49:44 -0700

    complete homework

--- W3D1/SQLite3_exercise/plays.rb
+++ W3D1/SQLite3_exercise/plays.rb
@@ -13,7 +13,8 @@ end
 
 class Play
   attr_accessor :title, :year, :playwright_id
-
+  attr_reader :id
+  
   class << self #enter singleton scope
 
     def all
@@ -50,8 +51,9 @@ class Play
 
       plays.map { |play| Play.new(play) }
     end
-  end
+  end #exit singleton scope
 
+  #instance methods
   def initialize(options)
     @id = options['id']
     @title = options['title']
@@ -81,4 +83,75 @@ class Play
         id = ?
     SQL
   end
+end
+
+class Playwright
+  attr_accessor :name, :birth_year
+  attr_reader :id
+
+  class << self #singleton scope
+    def all
+      data = PlayDBConnection.instance.execute("SELECT * FROM plays")
+      data.map { |datum| Play.new(datum) }
+    end
+
+    def find_by_name(name)
+      person = PlayDBConnection.instance.execute(<<-SQL, name)
+        SELECT
+          *
+        FROM
+          playwrights
+        WHERE
+          name = ?
+      SQL
+      return nil if person.empty?
+
+      Playwright.new(person.first)
+    end
+  end #exit singleton scope
+
+  #instance methods
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def create
+    raise "#{self} already in database" if @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+    @id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year, @id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def get_plays
+    raise "#{self} not in database" unless @id
+    plays = PlayDBConnection.instance.execute(<<-SQL, @id)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        playwright_id = ?
+    SQL
+    plays.map { |play| Play.new(play) }
+  end
+
+
 end
