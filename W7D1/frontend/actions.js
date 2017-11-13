4078adee904f0995363ef30747bee68686b7d49c
LeoXugu <robinleoknauth@gmail.com>
2017-11-12 17:49:17 -0800

    completed W7D1

---
+++ W7D1/frontend/actions.js
@@ -0,0 +1,11 @@
+// import
+
+const selectCurrency = (baseCurrency, rates) => {
+  return {
+    type: "SWITCH_CURRENCY",
+    baseCurrency: baseCurrency,
+    rates: rates
+  };
+};
+
+export default selectCurrency;
